30176b684b9bf01ccd98854ea06d95715101968b
Tommy Vo <votommy@outlook.com>
2020-02-15 01:43:07 -0600

    "Coming Soon" message

---
+++ logic.js
@@ -0,0 +1,5 @@
+document.getElementById("copyrightInfo").innerHTML = "&copy; " + new Date().getFullYear() + " Peep. All rights reserved";
+            
+function transformHamburger() {
+    document.getElementById("hamburgerMenu").classList.toggle("xBTN");
+}
