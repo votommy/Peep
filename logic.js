2d7bf63c2ea30d366e4f74cd3319194ee0d19393
Tommy Vo <votommy@outlook.com>
2020-02-16 02:10:59 -0600

    Made working nav menu links

--- logic.js
+++ logic.js
@@ -1,7 +1,21 @@
 document.getElementById("copyrightInfo").innerHTML = "&copy; " + new Date().getFullYear() + " Peep. All rights reserved";
             
 function transformHamburger() {
-    document.getElementById("hamburgerMenu").classList.toggle("xBTN");
+    if(!document.getElementById("hamburgerMenu").classList.contains("xBTN")) {
+        showNavList();
+    }
+    else {
+        hideNavList();
+    }
+}
+
+function showNavList() {
+    document.getElementById("hamburgerMenu").classList.add("xBTN");
+    document.getElementById("navList").classList.add("navListVisible");
+}
+function hideNavList() {
+    document.getElementById("hamburgerMenu").classList.remove("xBTN");
+        document.getElementById("navList").classList.remove("navListVisible");
 }
 
 const peepMarker = L.icon({
@@ -9,12 +23,12 @@ const peepMarker = L.icon({
     shadowUrl: "image/markerShadow.png",
     iconSize: [22.5, 27],
     shadowSize: [25, 7.6],
-    iconAnchor: [11.25, 27], // point of the icon which will correspond to marker's location
+    iconAnchor: [11.25, 27],
     shadowAnchor: [11.25, 3],
-    popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
+    popupAnchor: [0, -13.5]
 });
             
-const mymap = L.map('mapid').setView([39.10345, -94.58311], 13);
+const mymap = L.map('mapid', {scrollWheelZoom: false}).setView([39.10345, -94.58311], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -25,6 +39,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoiczUzMTgyMyIsImEiOiJjazZucndiOWQweXF2M2xwYnh4aThzYzh6In0.JbIjPZx26xRfEIDKrlFPgg'
 }).addTo(mymap);
 
+//Sample data: events
 const chanceBday = L.marker([39.099418, -94.579942], {icon: peepMarker}).addTo(mymap);
 chanceBday.bindPopup("<b>Corey's Birthday!</b><br>Saturday, 7pm-11pm").openPopup();
 
@@ -34,7 +49,7 @@ sandwichShop.bindPopup("<b>Free sandwiches!</b><br>Today! 2pm - 3:30pm").openPop
 const charityBasketball = L.marker([39.086162, -94.577597], {icon: peepMarker}).addTo(mymap);
 charityBasketball.bindPopup("<b>Basketball game for charity</b><br>Tomorrow, 10am - 11:30am").openPopup();
 
-//For getting coordinates
+// For getting coordinates
 //function onMapClick(e) {
 //    alert("You clicked the map at " + e.latlng);
 //}
